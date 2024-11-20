fc0110490fc8bb811515405173e6d92f73831bdf
AQUINOVS <felippeaquino56@gmail.com>
2024-11-20 06:49:56 -0300

    Subindo projeto amitaf (Frontend)

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,22 +1,24 @@
 import './index.scss';
 
-export default function Login() {
+export default function Login () {
   return (
-      <div class="container">
-        <div class="login-box">
-            <img src="/assets/images/amitaf.png" alt="logo-amitaf" class="logo-amitaf"/> 
-                <h2>Conta - ADM</h2>
-            <input type="text" placeholder="CPF"/>
-            <input type="password" placeholder="Senha"/>
-            <button>Entrar</button>
-            <div class="redes-sociais">
-                <img src="https://via.placeholder.com/25" alt="Instagram"/> 
-                <img src="https://via.placeholder.com/25" alt="Whatsapp"/>
-                <img src="https://via.placeholder.com/25" alt="Facebook"/>
+    <div class="centraliza">
+        <div class="container">
+            <div class="login-box">
+                <img src="/assets/images/amitaf.png" alt="Logo" class="logo-amitaf" />
+                <h2>Login</h2>
+                <input type="text" placeholder="Usuário" />
+                <input type="password" placeholder="Senha" />
+                <button>Entrar</button>
+                <div class="redes-sociais">
+                    <img src="https://via.placeholder.com/25" alt="Facebook" />
+                    <img src="https://via.placeholder.com/25" alt="Twitter" />
+                    <img src="https://via.placeholder.com/25" alt="Google" />
+                </div>
+                <p>A moda passa,o estilo permanece</p>
             </div>
-            <p>A moda passa, o estilo permanece</p>
+            <div class="image-box"></div>
         </div>
-        <div class="image-box"></div>
     </div>
     )
 }
